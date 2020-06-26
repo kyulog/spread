14783b4fe6b446ce6cdf89f3720c58a69a2fa19b
YeoKyuLi <kyuli.yeo@gmail.com>
2020-06-26 22:38:36 +0900

    added exception

--- src/main/java/com/example/spread/domain/repository/RecivedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.spread.domain.repository;
-
-import com.example.spread.domain.entity.ReceivedEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RecivedRepository extends JpaRepository<ReceivedEntity, Integer> {
-
-}
